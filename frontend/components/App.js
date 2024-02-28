d7a73376f84cccd924850d754eab0d7a6070447c
Dio <v.m.stanis@gmail.com>
2024-02-28 21:59:06 +0300

    mv files

---
+++ frontend/components/App.js
@@ -0,0 +1,13 @@
+export default class App {
+  #voyages = [];
+
+  async init() {
+    // init add voyage modal
+    // event listener for add voyage button press
+    try {
+      // load and render voyages from Model
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
