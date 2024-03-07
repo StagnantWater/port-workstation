8c948387df0ac126681626449f3873e3e57ad612
Dio <v.m.stanis@gmail.com>
2024-03-07 16:02:35 +0300

    modal front

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,9 +4,42 @@ import Voyage from "./Voyage";
 export default class App {
   #voyages = [];
 
+  initAddVoyageModal() {
+    const addVoyageModal = document.getElementById('modal-add-voyage');
+
+    const cancelHandler = () => {
+      addVoyageModal.close();
+      // localStorage.setItem('addTaskTasklistID', '');
+      // addTaskModal.querySelector('.app-modal__input').value = '';
+    };
+
+    const okHandler = () => {
+      // const tasklistID = localStorage.getItem('addTaskTasklistID');
+      // const modalInput = addTaskModal.querySelector('.app-modal__input');
+
+      // if (tasklistID && modalInput.value) {
+      //   this.#tasklists.find(tasklist => tasklist.tasklistID === tasklistID)
+      //     .appendNewTask({ text: modalInput.value });
+      // }
+
+      cancelHandler();
+    };
+
+    addVoyageModal.querySelector('.modal-ok-btn').addEventListener('click', okHandler);
+    addVoyageModal.querySelector('.modal-cancel-btn').addEventListener('click', cancelHandler);
+    addVoyageModal.addEventListener('close', cancelHandler);
+  }
+
   async init() {
     // init add voyage modal
     // event listener for add voyage button press
+    this.initAddVoyageModal();
+
+    document.querySelector('.voyage-adder__btn')
+      .addEventListener('click', () => {
+          document.getElementById('modal-add-voyage').showModal();
+        });
+
     try {
       const voyages = await AppModel.getVoyages();
       for (const voyage of voyages) {
